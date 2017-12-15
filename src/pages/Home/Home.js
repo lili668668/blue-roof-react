9c1311908e70558cc6a9e9cfce07f3504a139d04
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-15 12:32:51 +0800

    make a home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 class Home extends Component {
   render() {
     return (
-        <div>部落格owo</div>
+        <div>
+          <h1 class='mdc-typography--display3 text-center'>部落格 owo</h1>
+          <div class='text-center'>
+            <a href='#' class='mdc-typography--display2 mdc-button' style={{ minHeight: 50 + 'px' }}>
+              文章列表
+            </a>
+          </div>
+        </div>
         )
   }
 }
