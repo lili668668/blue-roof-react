c42fa85faf64c127153ae88366b0e488bafe1889
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-15 18:00:25 +0800

    List.js can get data

--- src/pages/Post/List.js
+++ src/pages/Post/List.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react'
 
+
 class List extends Component {
+    constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/mock_posts')
+      .then(res => res.json())
+      .then((result) => {
+          this.setState({
+            isLoaded: true,
+            posts: result
+          })
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
   render() {
-    return (
-        <div>
-          文章列表
-        </div>
-        )
+    const { error, isLoaded, posts } = this.state
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+          <ul>
+          {posts.map(post => (
+              <li key={post.title}>
+              {post.title} {post.content}
+              </li>
+          ))}
+          </ul>
+          )
+    }
   }
 }
 
