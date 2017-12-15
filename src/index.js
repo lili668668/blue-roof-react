da726b3f6e6f4e3d88c5381525a0941c78f0f4ec
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-15 10:21:07 +0800

    Set router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Router, Route, IndexRouter } from 'react-router'
+import { HashRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
+import App from './pages/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter>
+      <Route path="/" component={App} />
+    </HashRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
