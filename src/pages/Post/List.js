287bc8343a1ca4f985bac71e1b1ecc6d1d95d6a2
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-15 18:13:08 +0800

    Add 'Add post' button

--- src/pages/Post/List.js
+++ src/pages/Post/List.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
+import { Title } from '../../components'
+import AddLogo from './ic_note_add_white_24px.svg'
+import './List.css'
 
 class List extends Component {
     constructor(props) {
@@ -37,13 +41,21 @@ class List extends Component {
       return <div>Loading...</div>
     } else {
       return (
-          <ul>
-          {posts.map(post => (
-              <li key={post.title}>
-              {post.title} {post.content}
-              </li>
-          ))}
-          </ul>
+          <div>
+            <Title title='文章列表' />
+            <ul>
+            {posts.map(post => (
+                <li key={post.title}>
+                {post.title} {post.content}
+                </li>
+            ))}
+            </ul>
+            <Link to='/post/add' className='mdc-fab material-icons app-fab--absolute'>
+              <span className='mdc-fab__icon'>
+                <img src={AddLogo} alt='Add' />
+              </span>
+            </Link>
+          </div>
           )
     }
   }
