3b443f07bbb214c87e1ad0ea0360ab86e128870a
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-15 18:32:48 +0800

    remove useless-constructor

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Title extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   static propTypes = {
     title: PropTypes.string
   }
