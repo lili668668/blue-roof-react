77dee6419a65cbafd3084e5c3ed7c0254b0a48c1
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-15 12:32:38 +0800

    make a header

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class App extends Component {
+  static propTypes = {
+    children: PropTypes.object
+  }
+
   render() {
     return (
         <div>
-          www
+          <header class='mdc-toolbar'>
+            <div class='mdc-toolbar__row'>
+              <section class='mdc-toolbar__section mdc-toolbar__section--align-start'>
+                <span class='mdc-toolbar__title'>
+                  Blue Roof
+                </span>
+              </section>
+            </div>
+          </header>
+          {this.props.children}
         </div>
         )
   }
