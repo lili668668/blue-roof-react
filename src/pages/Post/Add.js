ef171ce333a60d1810a75e2aba4e99150ce11efc
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-18 17:00:42 +0800

    ready to write it

--- src/pages/Post/Add.js
+++ src/pages/Post/Add.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
+import { Title } from '../../components'
+
 class Add extends Component {
+
   render() {
     return (
-        <div>
-          加入新文章
-        </div>
+          <div>
+            <div className='mdc-layout-grid'>
+              <div className='mdc-layout-grid__inner'>
+                <div className='mdc-layout-grid__cell'></div>
+                <div className='mdc-layout-grid__cell'>
+                  <Title title='加入文章' />
+                </div>
+                <div className='mdc-layout-grid__cell'></div>
+              </div>
+            </div>
+          </div>
         )
   }
 }
