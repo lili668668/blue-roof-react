23174109234fc19d7903822921d22420523ea0e9
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-29 12:43:15 +0800

    add unit test

---
+++ test/actions/UiActionsSpec.js
@@ -0,0 +1,56 @@
+import { expect } from 'chai'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+import {
+  LOADING,
+  LOADED,
+  NO_ERROR,
+  ERROR
+} from '../../src/constants/actionTypes'
+import { UiState } from '../../src/constants/models'
+import { UiActions } from '../../src/actions'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+describe('actions/UiActions', () => {
+  describe('#Loading()', () => {
+    it('create LOADING', () => {
+      const store = mockStore(UiState)
+      store.dispatch(UiActions.Loading())
+      const actual = store.getActions()
+      expect(actual).to.deep.equal([{ type:LOADING }])
+    })
+  })
+
+  describe('#Loaded()', () => {
+    it('create LOADED', () => {
+      const store = mockStore(UiState)
+      store.dispatch(UiActions.Loaded())
+      const actual = store.getActions()
+      expect(actual).to.deep.equal([{ type:LOADED }])
+    })
+  })
+
+  describe('#NoError()', () => {
+    it('create NO_ERROR', () => {
+      const store = mockStore(UiState)
+      store.dispatch(UiActions.NoError())
+      const actual = store.getActions()
+      expect(actual).to.deep.equal([{ type:NO_ERROR }])
+    })
+  })
+
+  describe('#Error()', () => {
+    it('create ERROR', () => {
+      const store = mockStore(UiState)
+      const str = 'There are some errors.'
+      const error = { message: str }
+      store.dispatch(UiActions.Error(error))
+      const actual = store.getActions()
+      expect(actual).to.deep.equal([{ type:ERROR, payload: { msg: str } }])
+    })
+  })
+})
+
---
+++ test/mocks/getPostsMock.js
@@ -0,0 +1,1 @@
+export default {"posts":[{"_id":"5a4525035fbc48004492f414","title":"我覺得好累OAO","author":"ballfish","content":"心覺得累OAO","__v":0,"created":"2017-12-28T17:08:19.009Z"}]}
