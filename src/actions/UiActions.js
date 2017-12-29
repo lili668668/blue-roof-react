6cba80c0935d5efe8ff5c2bc48b341cbba16c0f7
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-29 10:29:26 +0800

    remove console.log

--- src/actions/UiActions.js
+++ src/actions/UiActions.js
@@ -11,7 +11,6 @@ export const Loaded = createAction(LOADED)
 export const NoError = createAction(NO_ERROR)
 export const Error = (error) => {
   return (dispatch) => {
-    console.log(error)
     dispatch({ type: ERROR, payload: { msg: error.message } })
   }
 }
