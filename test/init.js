13e06af744763ad615d580bc244494a8477d0ca4
Tzu-Yin Hong <lili668668@gmail.com>
2018-01-02 20:03:57 +0800

    the setting of jsdom has problem

--- test/init.js
+++ test/init.js
@@ -1,11 +1,11 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import jsdom, { JSDOM } from 'jsdom'
+//import jsdom, { JSDOM } from 'jsdom'
 
 configure({ adapter: new Adapter() })
 
-if (typeof document === 'undefined') {
+/*if (typeof document === 'undefined') {
   global.document = (new JSDOM('<!doctype html><html><head></head><body></body></html>')).window.document
   global.window = document.defaultView
   global.navigator = global.window.navigator
-}
+}*/
