7ea549917b688a9b9884792dd6936c37c125f7fe
Tzu-Yin Hong <lili668668@gmail.com>
2018-01-03 23:21:34 +0800

    modify the describe

--- src/test/__tests__/components/TitleSpec.js
+++ src/test/__tests__/components/TitleSpec.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
 
-import Title from '../../../components/Title'
+import { Title } from '../../../components'
 
 describe('components/<Title />', () => {
-  it('send text as children to <Title /> should become #title text', () => {
+  it('send text as children to <Title /> should become text', () => {
     const titleTag = shallow(<Title>Hello World</Title>)
     expect(titleTag.childAt(0).text()).to.equal('Hello World')
   })
